9fcdca57e4883f1edd7f838018e8520cd9d14ac6
Mauricio Caterino <mauricio_caterino@outlook.com>
2022-01-18 09:48:10 -0400

    added book service, book controller and http crud request

---
+++ my-books/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Models;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksService _service;
+
+        public BooksController(BooksService service)
+        {
+            _service = service;
+        }
+
+        //First endpoint
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookVM book)
+        {
+            _service.Add(book);
+            return Ok();
+        }
+
+        //Second endpoint / GetallBooks
+        [HttpGet("get-all-books")]
+        public IActionResult  GetAllBooks()
+        {
+            var allBooks = _service.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        //Third endpoint / GetBookById/1
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _service.GetBookById(id);
+            return Ok(book);
+        }
+        
+        //Fourth endpoint / UpdateBookById/1
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody]BookVM book)
+        {
+            var updatedBook = _service.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        //Fifth endpoint / DeleteBookById/1
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _service.DeleteBookById(id);
+            return Ok();
+        }
+    }
+}
---
+++ my-books/Data/Services/BooksService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using my_books.Data.Models;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Services
+{
+    public class BooksService
+    {
+        private readonly AppDbContext _context;
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public void Add(BookVM book)
+        {
+            var _book = new Book() 
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value : null,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+        
+        public Book GetBookById(int id)
+        {
+            var result = _context.Books.FirstOrDefault(b => b.Id == id);
+            return result;
+        }
+
+        public Book UpdateBookById(int id, BookVM book)
+        {
+            var result = _context.Books.FirstOrDefault(b => b.Id == id);
+            
+            if (result != null)
+            {
+                result.Title = book.Title;
+                result.Description = book.Description;
+                result.IsRead = book.IsRead;
+                result.DateRead = book.IsRead ? book.DateRead.Value : null;
+                result.Rate = book.IsRead ? book.Rate.Value : null;
+                result.Genre = book.Genre;
+                result.Author = book.Author;
+                result.CoverUrl = book.CoverUrl;
+                
+                _context.SaveChanges();
+            }
+          
+            return result;
+        }
+
+        public void DeleteBookById(int id)
+        {
+            var result = _context.Books.FirstOrDefault(b => b.Id == id);
+            if (result != null)
+            {
+                _context.Books.Remove(result);
+                _context.SaveChanges(); 
+            }
+        }
+
+    }
+}
